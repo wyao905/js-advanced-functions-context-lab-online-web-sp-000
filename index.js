6c415187e9e1eebcd8fc02897787f42309f53ae2
Warren Yao <warrenyca905@gmail.com>
2020-01-30 22:36:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,38 @@ function createEmployeeRecords(employees) {
     return employees.map(createEmployeeRecord)
 }
 
+function createTimeInEvent(employee, timeString) {
+    let indate = timeString.split(" ")[0]
+    let intime = timeString.split(" ")[1]
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(intime),
+        date: indate
+    })
+    return employee
+}
+
+function createTimeOutEvent(employee, timeString) {
+    let outdate = timeString.split(" ")[0]
+    let outtime = timeString.split(" ")[1]
+    employee.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(outtime),
+        date: outdate
+    })
+    return employee
+}
+
+function hoursWorkedOnDate(employee, date) {
+    let hourIn = employee.timeInEvents.find( e => e.date === date ).hour / 100
+    let hourOut = employee.timeOutEvents.find( e => e.date === date).hour / 100
+    return hourOut - hourIn
+}
+
+function wagesEarnedOnDate(employee, date) {
+    return hoursWorkedOnDate(employee, date) * employee.payPerHour
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -35,3 +67,4 @@ let allWagesFor = function () {
 
     return payable
 }
+
