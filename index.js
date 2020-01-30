987cf77c095b499708c0f6215fdcccd52cd4dcd6
Warren Yao <warrenyca905@gmail.com>
2020-01-30 23:17:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function hoursWorkedOnDate(date) {
 }
 
 function wagesEarnedOnDate(date) {
-    return this.hoursWorkedOnDate(date) * this.payPerHour
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
 }
 
 let allWagesFor = function () {
